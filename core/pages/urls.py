a9b68236242ca5ba649a1662090d9c93756ceafc
GodsplanELot <elotgodsplan2@gmail.com>
2025-01-05 16:29:13 +0100

    created the haspower allocation page

--- core/pages/urls.py
+++ core/pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'pages'
+
 urlpatterns = [
     path('', views.index, name='index'),  # Home page
+    path('dashboard/', views.dashboard, name='dashboard'),  # Dashboard
+    path('hashPowerAll/', views.hashPowerAll, name='hashPowerAll'),  # hashPowerAll
 ]
--- core/pages/views.py
+++ core/pages/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')  # Render the base template for now
+    return render(request, 'pages/index.html')  # Render the index template.
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')  # Render the dashboard template.
+def hashPowerAll(request):
+    return render(request, 'pages/hashPowerAll.html')  # Render the dashboard template.
