5214ed0c12c2d772ce0ce41c9253052df68b574e
unknown <elotgodsplan2@gmail.com>
2025-02-08 16:12:09 +0100

    added url patterns

--- core/support/urls.py
+++ core/support/urls.py
@@ -2,3 +2,11 @@ from django.urls import path
 from . import views
 
 #url patterns
+
+app_name = 'support'
+
+urlpatterns = [
+    path('inbox/', views.inbox, name='inbox'),
+    path('sent/', views.sent_messages, name='sent_messages'),
+    path('send/', views.send_message, name='send_message'),
+]
