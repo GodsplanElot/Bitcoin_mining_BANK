5995f9989ee8c2ce4ec40897ac37be4b472fb11e
unknown <elotgodsplan2@gmail.com>
2025-02-08 14:56:48 +0100

    Import required moules to the support views

--- core/support/views.py
+++ core/support/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import MessageForm
+from .models import Message
 
 # Create your views here.
