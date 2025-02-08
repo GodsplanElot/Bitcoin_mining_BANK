05c2449b5065e0f37f52979b457b4d9934dcd998
unknown <elotgodsplan2@gmail.com>
2025-02-08 16:32:47 +0100

    Modify support forms.py

--- core/support/forms.py
+++ core/support/forms.py
@@ -5,3 +5,10 @@ class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ['recipient', 'subject', 'body']
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)  # Get the current user from the view
+        super().__init__(*args, **kwargs)
+        if user:
+            # Prevent users from selecting themselves as recipient
+            self.fields['recipient'].queryset = User.objects.exclude(id=user.id)
