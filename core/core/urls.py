165209c611afec3f1f06d451ea9bd4825f939db5
unknown <elotgodsplan2@gmail.com>
2025-02-10 00:24:30 +0100

    Reposition 404 error logic line of code

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
+from pages.views import custom_404_view  # Import your custom 404 view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +32,9 @@ urlpatterns = [
     
 ]
 
+
+# Assign the custom 404 view (MUST be placed after urlpatterns)
+handler404 = custom_404_view
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
