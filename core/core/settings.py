9f3ae4355d931965f3c2015e030bafb69a2e0634
unknown <elotgodsplan2@gmail.com>
2025-02-10 00:15:34 +0100

    Fix Bug, added allowed

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jcksnq1n444m0ykqpl*z+is$+9subh#zj@1btctym@9elr3)y9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 LOGIN_URL = 'login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
