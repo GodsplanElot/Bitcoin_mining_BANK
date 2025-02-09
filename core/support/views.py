15bbb0cb3ca31e3cc49db9a5363ec1754bc6503b
unknown <elotgodsplan2@gmail.com>
2025-02-09 23:25:15 +0100

    Add decorator for access only to superuser

--- core/support/views.py
+++ core/support/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
 from .forms import MessageForm
 from .models import Message
